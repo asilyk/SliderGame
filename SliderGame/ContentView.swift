77d7ab1ed4b1a3777a68c5e475e6a03c4e01ffd3
fabet <asilyk112@gmail.com>
2022-05-02 19:30:11 +0300

    Initial commit

--- SliderGame/ContentView.swift
+++ SliderGame/ContentView.swift
@@ -8,14 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var targetValue = Int.random(in: 0...100)
+    @State private var currentValue = Double.random(in: 0...100)
+    @State private var score = 100
+    @State private var isAlertPresented = false
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing: 32) {
+            Text("Подвиньте слайдер, как можно ближе к: \(targetValue)")
+                .frame(height: 100)
+            HStack {
+                Text("0")
+                UIKitSlider(value: $currentValue, alpha: score)
+                    .onAppear {
+                        score = computeScore()
+                    }
+                    .onChange(of: currentValue) { _ in
+                        score = computeScore()
+                }
+                Text("100")
+            }
+
+            Button("Проверь меня!") {
+                isAlertPresented = true
+            }
+            .alert( "Your score", isPresented: $isAlertPresented, actions: {}) {
+                Text("\(score)")
+            }
+
+            Button("Начать заново") {
+                targetValue = Int.random(in: 0...100)
+            }
+            .onChange(of: targetValue) { _ in
+                score = computeScore()
+            }
+        }
+        .padding()
+    }
+
+    private func computeScore() -> Int {
+        let difference = abs(targetValue - lround(currentValue))
+        return 100 - difference
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("IPhone 13")
     }
 }
---
+++ SliderGame/UIKitSlider.swift
@@ -0,0 +1,61 @@
+//
+//  UIKitSlider.swift
+//  SliderGame
+//
+//  Created by Alexander on 02.05.2022.
+//
+
+import SwiftUI
+
+struct UIKitSlider: UIViewRepresentable {
+    @Binding var value: Double
+    let alpha: Int
+
+    func makeUIView(context: Context) -> UISlider {
+        let slider = UISlider()
+        slider.minimumValue = 0
+        slider.maximumValue = 100
+        slider.thumbTintColor = initRed(with: alpha)
+
+        slider.addTarget(
+            context.coordinator,
+            action: #selector(Coordinator.valueChanged),
+            for: .valueChanged
+        )
+
+        return slider
+    }
+    
+    func updateUIView(_ uiView: UISlider, context: Context) {
+        uiView.value = Float(value)
+        uiView.thumbTintColor = initRed(with: alpha)
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(value: $value)
+    }
+
+    private func initRed(with alpha: Int) -> UIColor {
+        UIColor(red: 1, green: 0, blue: 0, alpha: Double(alpha)/100)
+    }
+}
+
+extension UIKitSlider {
+    final class Coordinator: NSObject {
+        @Binding var value: Double
+
+        init(value: Binding<Double>) {
+            self._value = value
+        }
+
+        @objc func valueChanged(_ sender: UISlider) {
+            value = Double(sender.value)
+        }
+    }
+}
+
+struct UIKitSlider_Previews: PreviewProvider {
+    static var previews: some View {
+        UIKitSlider(value: .constant(12), alpha: 50)
+    }
+}
